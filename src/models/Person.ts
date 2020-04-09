42e80cbac8a234724eddf79e0faf8cc1f28e2442
Sabin Mendiguren <sabin.mendiguren@gmail.com>
2020-04-09 12:31:34 +0200

    Fix gravity interactions

--- src/models/Person.ts
+++ src/models/Person.ts
@@ -1,31 +1,6 @@
 const rad = 2;
 const piRad = 2 * Math.PI;
 
-interface WorldInterface {
-    height: number;
-    width: number;
-    pArrayNotSelf: Array<Person>;
-}
-
-const calcGravity = (p: Person, allObjs: Array<Person>) => {
-    allObjs.forEach(function (obj2) {
-        var diffX = obj2.x - p.x;
-        var diffY = obj2.y - p.y;
-        var distSquare = diffX * diffX + diffY * diffY;
-        var dist = Math.sqrt(distSquare);
-        if (dist - 5 > rad / 2 + rad / 2) {
-            var totalForce = 5 / distSquare;
-            p.dx += totalForce * diffX / dist;
-            p.dy += totalForce * diffY / dist;
-        } else {
-            // var tempX = (p.dx + obj2.dx) / 2;
-            // var tempY = (p.dy + obj2.dy) / 2;
-            // p.dx = tempX; obj2.dx = tempX;
-            // p.dy = tempY; obj2.dy = tempY;
-        }
-    });
-};
-
 export class Person {
     x: number;
     y: number;
@@ -37,14 +12,9 @@ export class Person {
         this.dx = dx;
         this.dy = dy;
     }
-    step({ height, width, pArrayNotSelf }: WorldInterface) {
-        calcGravity(this, pArrayNotSelf);
-        if (this.x > width || this.x < 0) {
-            this.dx = this.dx * -1;
-        }
-        if (this.y > height || this.y < 0) {
-            this.dy = this.dy * -1;
-        }
+    step(drag: number = 1) {
+        this.dx = this.dx * drag;
+        this.dy = this.dy * drag;
         this.x += this.dx;
         this.y += this.dy;
     }
--- src/models/World.ts
+++ src/models/World.ts
@@ -1,12 +1,58 @@
 import { Person } from './Person';
 
-const numberOfP = 100;
+const numberOfP = 500;
+const gForce = 0
+const boundElasticity = 0.90;
+const drag = 1;
+
+const totalEElement = document.getElementById('totalE') as HTMLSpanElement;
+
+const calcGravity = (allPeople: Array<Person>) => {
+    const rad = 2;
+    for (let i = 0; i < allPeople.length; i++) {
+        for (let j = i + 1; j < allPeople.length; j++) {
+            if (i !== j) {
+                var diffX = allPeople[j].x - allPeople[i].x;
+                var diffY = allPeople[j].y - allPeople[i].y;
+                var distSquare = diffX * diffX + diffY * diffY;
+                var dist = Math.pow(distSquare, 0.5);
+                if (dist - 1 > rad / 2 + rad / 2) {
+                    var totalForce = gForce / distSquare;
+                    const deltaX = totalForce * diffX / dist;
+                    const deltaY = totalForce * diffY / dist;
+                    allPeople[i].dx += deltaX;
+                    allPeople[i].dy += deltaY;
+                    allPeople[j].dx -= deltaX;
+                    allPeople[j].dy -= deltaY;
+                } else {
+                    // var tempX = (p.dx + obj2.dx) / 2;
+                    // var tempY = (p.dy + obj2.dy) / 2;
+                    // p.dx = tempX; obj2.dx = tempX;
+                    // p.dy = tempY; obj2.dy = tempY;
+                }
+            }
+        }
+    }
+};
+
+const calcBounds = (allPeople: Array<Person>, height: number, width: number) => {
+    allPeople.forEach((p) => {
+        if (p.x > width || p.x < 0) {
+            p.dx = p.dx * (-1 * boundElasticity);
+        }
+        if (p.y > height || p.y < 0) {
+            p.dy = p.dy * (-1 * boundElasticity);
+        }
+    });
+};
+
 
 export class World {
     canvas: HTMLCanvasElement;
     ctx: CanvasRenderingContext2D;
     pArray: Array<Person>;
     started: boolean;
+    totalElergy: number;
     constructor(height: number = 500, width: number = 500) {
         this.started = false;
         this.canvas = document.getElementById('worldCanvas') as HTMLCanvasElement;
@@ -14,6 +60,7 @@ export class World {
         this.canvas.width = width;
         this.ctx = this.canvas.getContext('2d');
         this.pArray = [];
+        this.totalElergy = null;
         for (let i = 0; i < numberOfP; i++) {
             this.pArray.push(new Person(Math.random() * width, Math.random() * height, (Math.random() * 1) - 1 / 2, (Math.random() * 1) - 1 / 2));
         }
@@ -22,8 +69,12 @@ export class World {
         if (!this.started) {
             return;
         }
+        calcBounds(this.pArray, this.canvas.height, this.canvas.width);
+        calcGravity(this.pArray);
+        this.totalElergy = 0;
         this.pArray.forEach((p) => {
-            p.step({ height: this.canvas.height, width: this.canvas.width, pArrayNotSelf: this.pArray.filter(p2 => p2 !== p) });
+            this.totalElergy += Math.sqrt(Math.pow(p.dx, 2) + Math.pow(p.dy, 2));
+            p.step(drag);
         });
         this.draw();
         window.requestAnimationFrame(this.step.bind(this));
@@ -33,6 +84,7 @@ export class World {
         this.pArray.forEach((p) => {
             p.draw(this.ctx);
         });
+        totalEElement.innerHTML = String(this.totalElergy);
     }
     start() {
         if (this.started) {
